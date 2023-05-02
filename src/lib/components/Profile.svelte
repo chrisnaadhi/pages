399f526cc4e1286a0c1a12e2eb3c19ce7daa4408
chrisnaadhi <chrisnaadhip@gmail.com>
2023-05-02 18:54:30 +0700

    add list of web

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -1,30 +1,36 @@
 <script lang="ts">
+  import WebList from "$lib/components/WebList.svelte";
+
   export let myName: string;
 </script>
 
 <section class="section-wrapper">
-  <div class="max-w-70 text-right">
-    <h4>{myName}</h4>
-    <p class="text-xs text-dark dark:text-white">
-      I'm Chrisna, Full-time Learner. Aspiring Web Developer and Enthusiast of
-      IT in Library & Information Science.
-    </p>
-    <a href="/about" class="text-sm text-orange no-underline">About me..</a>
+  <div class="flex items-center justify-center gap-5 my-4">
+    <div class="max-w-70 text-right">
+      <h4>{myName}</h4>
+      <p class="text-xs text-dark dark:text-white">
+        I'm Chrisna, Full-time Learner. Aspiring Web Developer and Enthusiast of
+        IT in Library & Information Science.
+      </p>
+      <a href="/about" class="text-sm text-orange no-underline">About me..</a>
+    </div>
+    <div>
+      <img
+        src="/images/foto-profil.webp"
+        alt="Foto Profil"
+        width="128px"
+        height="128px"
+        class="rounded-full"
+      />
+    </div>
   </div>
-  <div>
-    <img
-      src="/images/foto-profil.webp"
-      alt="Foto Profil"
-      width="128px"
-      height="128px"
-      class="rounded-full"
-    />
-  </div>
+
+  <WebList />
 </section>
 
 <style>
   .section-wrapper {
-    --at-apply: flex items-center justify-center gap-5 my-4 bg-white dark:bg-dark rounded-xl mt-5 py-3;
+    --at-apply: bg-white dark:bg-dark rounded-xl mt-5 py-3;
   }
 
   h4 {
---
+++ src/lib/components/WebList.svelte
@@ -0,0 +1,40 @@
+<section class="wrapper">
+  <h5>Actively Maintained Website</h5>
+  <div class="web-list">
+    <a href="https://sajiwafoundation.org" target="_blank">
+      <div class="btn-block">Sajiwa Foundation</div>
+    </a>
+    <a href="https://kandaga-beta.vercel.app" target="_blank">
+      <div class="btn-block">Kandaga Unpad</div>
+    </a>
+    <a href="https://ngadongeng.com" target="_blank">
+      <div class="btn-block">Pabukon Ngadongeng</div>
+    </a>
+  </div>
+</section>
+
+<style>
+  .wrapper {
+    --at-apply: py-3;
+  }
+
+  h5 {
+    --at-apply: text-orange dark:text-white;
+  }
+
+  .web-list {
+    --at-apply: flex flex-col sm:flex-row items-center justify-center gap-5 pt-2;
+  }
+
+  a {
+    --at-apply: no-underline;
+  }
+
+  .btn-block {
+    --at-apply: bg-dark-50 min-w-60 p-2 rounded text-white shadow transition-all-500;
+  }
+
+  .btn-block:hover {
+    --at-apply: transition-all-500 font-600 bg-orange text-white shadow-orange-3 shadow-md;
+  }
+</style>
